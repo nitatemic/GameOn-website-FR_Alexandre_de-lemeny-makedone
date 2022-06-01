e3e06f10fe9153122e2b3a8403ef58136c16b0c4
nitatemic <alexandre@nitatemic.dev>
2022-06-01 20:29:19 +0200

    CheckMail + test

--- starterOnly/js/test/modal.test.js
+++ starterOnly/js/test/modal.test.js
@@ -1,5 +1,5 @@
 function TestCheckMail() {
-	if (CheckMail("this.is.a@mail.adress.dev") && CheckMail("This.isn't.an@adresse_mail.dev") {
+	if (CheckMail("this.is.a@mail.adress.dev") && CheckMail("This.isn't.an@adresse_mail.dev")) {
 		console.log("Test passed");
 	} else {
 		console.log("Test failed (CheckMail() function)");
